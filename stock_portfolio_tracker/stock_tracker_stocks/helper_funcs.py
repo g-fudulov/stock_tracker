027b84a179d434c9450f23b6127ea9bc71dbc3c9
g-fudulov <fudulov.georgi@gmail.com>
2023-09-17 13:52:52 +0300

    Added sell view and transaction logs

---
+++ stock_portfolio_tracker/stock_tracker_stocks/helper_funcs.py
@@ -0,0 +1,50 @@
+import api_config
+import requests
+
+
+def check_stock_exists(symbol):
+    querystring = {"country": "United States", "symbol": f"{symbol}", "format": "json"}
+
+    try:
+        # Make a GET request to the API
+        response = requests.get(api_config.url, headers=api_config.headers, params=querystring)
+
+        if response.status_code == 200:
+            # Parse the JSON response
+            parsed_response = response.json()
+
+            # Check if the response contains valid stock data
+            if not parsed_response['data']:
+                return False  # Does not exist
+            else:
+                return parsed_response  # Exists
+
+        else:
+            # Handle API request error (e.g., API key is invalid)
+            return False
+
+    except requests.exceptions.RequestException:
+        # Handle network-related errors
+        return False
+
+
+def calculate_avg_price_per_share_buy(existing_quantity, bought_quantity, existing_avg_price, bought_price):
+    total_quantity = existing_quantity + bought_quantity
+    total_cost = ((existing_quantity * existing_avg_price) + (bought_quantity * bought_price))
+    average_purchase_price = total_cost / total_quantity
+
+    return average_purchase_price, total_quantity
+
+
+def calculate_realised_pnl(existing_pnl, existing_avg_price, selling_price, selling_quantity):
+    realised_pnl = (selling_price - existing_avg_price) * selling_quantity
+    new_pnl = existing_pnl + realised_pnl
+
+    return new_pnl
+
+
+def update_quantity(existing_quantity, selling_quantity):
+    remaining_quantity = existing_quantity - selling_quantity
+    new_quantity = remaining_quantity
+
+    return new_quantity
--- stock_portfolio_tracker/stock_tracker_stocks/urls.py
+++ stock_portfolio_tracker/stock_tracker_stocks/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('details/<int:portfolio_pk>/', views.PortfolioDetails.as_view(), name='portfolio_details'),
     path('add-position/<int:portfolio_pk>/', views.add_stock_to_portfolio, name='add_stock'),
     path('buy/<int:portfolio_pk>/<str:stock_symbol>/', views.buy_stock, name='buy_stock'),
-    # path('sell/<int:portfolio_pk>/<str:stock_symbol>/', views.sell_stock, name='buy_stock'),
+    path('sell/<int:portfolio_pk>/<str:stock_symbol>/', views.sell_stock, name='sell_stock'),
     path('remove/<int:portfolio_pk>/<str:stock_symbol>/', views.RemoveStock.as_view(), name='remove_stock'),
+    path('sales/<int:profile_pk>/<int:portfolio_pk>/', views.SalesDetails.as_view(), name='sales_details'),
+    path('buys/<int:profile_pk>/<int:portfolio_pk>/', views.BuysDetails.as_view(), name='buys_details'),
 ]
-
