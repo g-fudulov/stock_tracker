d3f500fa1d962bdd7ba7d0516665539be2320fd5
g-fudulov <fudulov.georgi@gmail.com>
2023-09-23 10:14:32 +0300

    Finished backend

--- stock_portfolio_tracker/stock_tracker_stocks/models.py
+++ stock_portfolio_tracker/stock_tracker_stocks/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
 class Portfolio(models.Model):
     owner = models.OneToOneField(Profile, on_delete=models.CASCADE)
     stocks = models.ManyToManyField("Stock", through='PortfolioItem')
-    realised_pnl = models.FloatField(default=0)
+    realised_pnl = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"Portfolio of {self.owner.user.email}"
@@ -24,8 +24,8 @@ class Portfolio(models.Model):
 class PortfolioItem(models.Model):
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
     stock = models.ForeignKey("Stock", on_delete=models.CASCADE)
-    quantity = models.FloatField()
-    average_purchase_price = models.FloatField()
+    quantity = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    average_purchase_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"{self.stock.symbol} - {self.quantity} shares"
@@ -34,7 +34,7 @@ class PortfolioItem(models.Model):
 class Stock(models.Model):
     symbol = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=100)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return self.symbol
@@ -52,5 +52,5 @@ class Buy(models.Model):
     initiator = models.ForeignKey(Profile, on_delete=models.CASCADE)
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
-    quantity_bought = models.FloatField(default=0)
+    quantity_bought = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     buy_price = models.DecimalField(max_digits=10, decimal_places=2)
